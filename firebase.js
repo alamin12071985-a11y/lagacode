a9fa76b72bc8197524be73005f2625ee5a9ff3f0
LagaTech <alamin12071985@gmail.com>
2026-03-27 12:44:30 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD1PPDhogcw7fBu27PkO1iuMfGFLUwMN70",
   authDomain: "fir-55206.firebaseapp.com",
   databaseURL: "https://fir-55206-default-rtdb.firebaseio.com",
-  projectId: "fir-55206",v
+  projectId: "fir-55206",
   storageBucket: "fir-55206.firebasestorage.app",
   messagingSenderId: "24586463698",
   appId: "1:24586463698:web:8b2f21073295ef4382400b",
