66888de045ace7944e98e0650591036dbeaee368
LagaTech <alamin12071985@gmail.com>
2026-04-01 17:00:06 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const { initializeApp } = require('firebase/app');
-const { getDatabase, ref, set, get, child, remove, push, onValue, serverTimestamp, query, orderByChild, equalTo } = require('firebase/database');
+const { getDatabase, ref, set, get, child, remove, push, update, serverTimestamp } = require('firebase/database');
 
 const firebaseConfig = {
   apiKey: "AIzaSyBScBRNNMxj1RqQtTy2V1PkmBBqb0ed4rA",
@@ -10,25 +10,10 @@ const firebaseConfig = {
   appId: "1:414251734880:web:095a38b98bd029c87ab005"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
-// ডাটাবেস রুট রেফারেন্স
 const dbRef = ref(database);
 
-// এক্সপোর্ট করা হচ্ছে
 module.exports = {
-  database,
-  dbRef,
-  ref,      // নতুন রেফারেন্স তৈরি করতে
-  set,      // ডাটা সেট করতে
-  get,      // ডাটা পড়তে
-  child,    // চাইল্ড নোড অ্যাক্সেস করতে
-  remove,   // ডাটা ডিলিট করতে
-  push,     // নতুন আইডি তৈরি করতে
-  query,    // কোয়েরি করতে
-  orderByChild,
-  equalTo,
-  serverTimestamp
+  database, dbRef, ref, set, get, child, remove, push, update, serverTimestamp
 };
